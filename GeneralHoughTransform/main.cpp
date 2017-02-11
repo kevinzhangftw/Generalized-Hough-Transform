4c5b9750ecd0047fc6522032ddfba5ddef496a30
Kai Zhang <kai@d207-023-177-053.wireless.sfu.ca>
2017-02-10 19:24:25 -0800

    Initial Commit

---
+++ GeneralHoughTransform/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GeneralHoughTransform
+//
+//  Created by Kai Zhang on 2017-02-10.
+//  Copyright © 2017 Kai Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
